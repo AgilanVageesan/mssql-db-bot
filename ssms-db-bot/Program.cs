876233f9b3866fe9d5f092cb968378cf5cc644b6
Agilan Vageesan <agilanvageesan@gmail.com>
2021-06-25 22:44:37 +0530

    feat(main): project checkin

---
+++ ssms-db-bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ssms_db_bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
