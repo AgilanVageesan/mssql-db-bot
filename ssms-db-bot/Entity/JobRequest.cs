4f78ae8415d72f191f17c0503ab1c6cc7046d9eb
Agilan Vageesan <agilanvageesan@gmail.com>
2021-06-25 23:09:04 +0530

    feat(main): Added MS SMO

---
+++ ssms-db-bot/Entity/JobRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace ssms_db_bot.Entity
+{
+    public class JobRequest
+    {
+        public string DbServer { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string JobName { get; set; }
+    }
+}
--- ssms-db-bot/Program.cs
+++ ssms-db-bot/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using ssms_db_bot.Entity;
 
 namespace ssms_db_bot
 {
@@ -6,7 +8,27 @@ namespace ssms_db_bot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length < 4)
+            {
+                Console.WriteLine("Please provide the requested details in this format : <dbserver> <username> <password> <jobname> <emailId>");
+                Thread.Sleep(150000);
+            }
+            else
+            {
+                var request = new JobRequest();
+                request.DbServer = args[0];
+                request.UserName = args[1];
+                request.Password = args[2];
+                request.JobName = args[3];
+
+                var toEmailAddress = args.Length == 5 ? args[4] : "dummy@gmail.com";
+
+            }
+
+
+        }
+        public static void TriggerSQLJob(JobRequest request)
+        {
         }
     }
 }
